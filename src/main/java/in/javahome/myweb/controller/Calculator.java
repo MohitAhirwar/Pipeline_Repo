8c8b0103495006ba7ee0d27844423ab27fecedd8
Mohit Ahirwar <66860602+MohitAhirwar@users.noreply.github.com>
2021-04-22 21:03:33 +0530

    Update Calculator.java

--- src/main/java/in/javahome/myweb/controller/Calculator.java
+++ src/main/java/in/javahome/myweb/controller/Calculator.java
@@ -1,14 +1,8 @@
 package in.javahome.myweb.controller;
 import java.util.List;
-/*
- * 
- */
+
 public class Calculator {
-	/*
-	 * @param i
-	 * @param j
-	 * @return int
-	 */
+
 	public int add(int i, int j){
 		return i+j;
 	}
