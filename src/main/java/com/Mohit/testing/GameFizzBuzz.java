f7d68fe2ca7ef0e96d3b5063c481c5d91e6532ee
Mohit Ahirwar <66860602+MohitAhirwar@users.noreply.github.com>
2021-04-22 12:48:50 +0530

    Update GameFizzBuzz.java

--- src/main/java/com/Mohit/testing/GameFizzBuzz.java
+++ src/main/java/com/Mohit/testing/GameFizzBuzz.java
@@ -1,14 +1,11 @@
-package com.Mohit.testing;
+package com.pipeline.testing;
 
-public class FizzBuzz {
-
-    public String play(int number) {
-
-        if (number == 0) throw new IllegalArgumentException("Number must not be 0");
-        if (number % 3 == 0) return "Fizz";
-        if (number % 5 == 0) return "Buzz";
-
-
-        return String.valueOf(number);
-    }
+public class Calculator 
+{
+	public int add(int i, int j){
+		return i+j;
+	}
+	public int multiply(int i, int j){
+		return i*j;
+	}
 }
